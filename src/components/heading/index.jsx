293ce1869569d15c07809423a2f1a06c86810d4b
Daniel Mora <daniel.mora@algotive.ai>
2023-07-05 10:49:51 -0600

    feat: add folders to components and pages

---
+++ src/components/heading/index.jsx
@@ -0,0 +1,7 @@
+export default function Heading() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Hola Mundo</h1>
+    </>
+  )
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+import Heading from '../../components/heading'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Heading />
+    </div>
+  )
+}
